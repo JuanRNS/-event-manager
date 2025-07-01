6dbbb5b8c436b951f9fb4c7916e78d0e90cf097f
Juan <juanramalho90@gmail.com>
2025-06-30 21:15:10 -0300

    primeiro commit

---
+++ event-manager-back-end/src/main/java/com/example/eventmanagerbackend/EventManagerBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerBackEndApplication.class, args);
+    }
+
+}
---
+++ event-manager-back-end/src/test/java/com/example/eventmanagerbackend/EventManagerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
