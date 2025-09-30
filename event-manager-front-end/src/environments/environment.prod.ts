79687f30113303b5341d2e4e43074b6bac844d1c
Juan <juanramalho90@gmail.com>
2025-09-30 14:01:27 -0300

    fix correcoes no http

--- event-manager-front-end/src/environments/environment.prod.ts
+++ event-manager-front-end/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    api: 'https://event-manager-production-bc44.up.railway.app'
+    api: 'https://event-manager-production-bc44.up.railway.app/api/'
 };
