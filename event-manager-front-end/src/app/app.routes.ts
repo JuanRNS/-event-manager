91867f374f63e8144af4c2cb88704299531ab158
Victor <victor_hugo_lacerda@hotmail.com>
2025-11-03 22:32:47 -0300

    att front - authguard

--- event-manager-front-end/src/app/app.routes.ts
+++ event-manager-front-end/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/views/login/login.component';
-import {  } from './core/guards/auth.guard';
+import { authGuard } from './core/guards/auth.guard';
 import { RegisterComponent } from './features/views/register/register.component';
 
 
@@ -21,27 +21,27 @@ export const routes: Routes = [
   { 
     path: 'dashboard', 
     loadComponent: () => import('./features/views/dashboard/dashboard.component').then((m) => m.DashboardComponent),
-    canActivate: []
+    canActivate: [authGuard]
   },
   { 
     path: 'party-registration', 
     loadComponent: () => import('./features/views/party-registration/party-registration.component').then((m) => m.PartyRegistrationComponent),
-    canActivate: []
+    canActivate: [authGuard]
   },
   { 
     path: 'event-components', 
     loadComponent: () => import('./features/views/event-components/event-components.component').then((m) => m.EventComponentsComponent),
-    canActivate: []
+    canActivate: [authGuard]
   },
   { 
     path: 'party-all-list', 
     loadComponent: () => import('./features/views/party-all-list/party-all-list.component').then((m) => m.PartyAllListComponent),
-    canActivate: []
+    canActivate: [authGuard]
   },
   {
     path: 'dashboard-date',
     loadComponent: () => import('./features/views/dashboard-date/dashboard-date.component').then((m) => m.DashboardDateComponent),
-    canActivate: []
+    canActivate: [authGuard]
   },
   { 
     path: '**', 
